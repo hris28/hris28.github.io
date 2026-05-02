24750eec2fe836eefaa8adfdc6cdef6f71129780
Hrishika Roychoudhury <hrishikarc@gmail.com>
2026-05-02 15:11:11 -0400

    layout & spacing

--- garden-src/quartz.layout.ts
+++ garden-src/quartz.layout.ts
@@ -75,8 +75,8 @@ export const defaultContentPageLayout: PageLayout = {
   ],
 
   right: [
-    Component.Graph(),
     Component.DesktopOnly(Component.TableOfContents()),
+    Component.Graph(),
     Component.Backlinks(),
   ],
 }
